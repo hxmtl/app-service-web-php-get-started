151b46ecbb2246d746cfad7b2435d0dac7bf7fc6
hxmtl <huxinmontreal@gmail.com>
2016-07-27 16:02:07 -0400

    do again, SVN in windows 10 without overlay

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,7 +68,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Congrats! You just created your first PHP web app in Azure App Service (Powered by CodeIgniter)!</h1>
+	<h1>Congrats! You just created your first PHP web app in Azure App Service (Powered by CodeIgniter, GitHub, SVN and VS code)!</h1>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
